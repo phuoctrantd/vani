487b347a5b2f4faef3e8a2de740b74bb5dde9da1
phuoctrantd <phuoctrantd@.com>
2022-07-03 22:32:23 +0700

    debug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Switch>
       <Route path="/Lotteria/:voucher_id" exact><Vani></Vani> </Route>
       <Route path="/Home/:voucher_id" exact ><Home /></Route>
-      <Route path="/Coin" exact ><Coin /></Route>
+      <Route path="/Coin/:voucher_id" exact ><Coin /></Route>
       <Route path="/Setting/:voucher_id" exact > <Setting /></Route>
       <Route path="*" exact > <NotFound /></Route>
       </Switch>
